f7208d8e69fa4e551838b98a636a3b0432fd91b8
Kanji Hara Neto <hkanjih@gmail.com>
2024-07-05 00:38:50 -0700

    final test

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,11 @@ st.write("The name on your Smoothie will be:", title)
 
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:", my_dataframe, max_selections=6)
@@ -31,7 +34,7 @@ if ingredients_list:
     ingredients_string = ' '.join(ingredients_list)
 
     for fruit_chosen in ingredients_list:
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
